105c157226a844be045c4617e6f084503d9e04c4
jonathan <jlopezv0324@gmail.com>
2023-11-09 11:01:50 -0700

    sets up jframe window with title

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -1,7 +1,24 @@
 package org.game;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() {
+        init();
+    }
+
+    private void init() {
+        setTitle("no name platformer");
+        setSize(1366,768);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }
+
+    public static void main(String[] args){
+        Main main = new Main();
+        main.setVisible(true);
     }
 }
